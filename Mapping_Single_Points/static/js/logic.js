298b27d89cd52c62b7eb29cf498e544040330bc7
Pascalduc <78885790+Pascalduc@users.noreply.github.com>
2021-05-16 22:46:56 -0400

    point_map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,16 +3,26 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([56.4, -102.2], 4);
-/* alternative to setview useful when we need to add multiple tile layers, or a background image of our map
-{
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  } */
+
+//  Add a Pin marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//  add a circle marker in meters
+let circle = L.circle([34.0522, -118.2437], {
+  radius: 300,
+  color: 'lightyellow',
+  fillColor: 'lightyellow'
+  }).addTo(map);
+
+// add a circleMarker alternative in pixel (resize with zoom)
+  L.circleMarker([37.3541, -121.9552], {
+  radius: 30,
+  color: 'black',
+  fillColor: '#ffffa1'
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 maxZoom: 18,
 accessToken: API_KEY
@@ -21,3 +31,6 @@ accessToken: API_KEY
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+// mapbox://styles/mapbox/dark-v10
